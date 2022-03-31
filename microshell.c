c3d8574370f0872c5fcd43dd9d8ea9d1385efce7
Fernando Ballesteros santamaria <fballest@c3r9s5.42madrid.com>
2022-03-31 11:18:15 +0200

    OK

--- microshell.c
+++ microshell.c
@@ -1,66 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   micro_shell.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fballest <fballest@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 11:16:22 by fballest          #+#    #+#             */
+/*   Updated: 2022/03/31 11:16:28 by fballest         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
-int ft_strlen(char *str)
+int     ft_strlen(char *str)
 {
-    int i;
-    i = 0;
-    while (str[i])
+    int i = 0;
+    while(str[i])
         i++;
-    return (i);
+    return(i);
 }
 
-void ft_error(void)
+void    ft_error(void)
 {
-    write(2,"Fatal Error.\n",ft_strlen("Fatal Error.\n"));
-    exit(1);
+    write(2, "error: fatal\n", ft_strlen("error: fatal\n"));
+    exit (1);
 }
 
-void ft_msg(char *str, char *com)
+void    ft_msg(char *str, char *com)
 {
     write(2, str, ft_strlen(str));
     write(2, com, ft_strlen(com));
-    write(2, "\n",1);
-    return;
+    write(2, "\n", 1);
 }
 
 void    ft_cd(char **argv)
 {
-    
-    int i = 1;
+    int i = 0;
     while (argv[i])
         i++;
-    if (i < 2)
-    {
-        write(2, "error: cd: bad arguments\n", ft_strlen("error: cd: bad arguments\n"));
-    }
-    else if (chdir(argv[1]))
+    if (i != 2)
+        write (2, "error: cd: bad arguments\n", ft_strlen("error: cd: bad arguments\n"));
+    if (chdir(argv[1]))
         ft_msg("error: cd: cannot change directory to ", argv[1]);
+    
 }
 
 int main(int argc, char **argv, char **env)
 {
-    int j = 1, j_prev, pipes[2], status, type;
-    char **command;
-    pid_t pid;
-
-    while(j < argc)
+    int     j = 1, j_prev, pipes[2], status, type;
+    char    **command;
+    pid_t   pid;
+    while (j < argc)
     {
         j_prev = j;
         type = 0;
-        while (j<argc && strcmp(argv[j],"|") && strcmp(argv[j],";"))
+        while (j < argc && strcmp(argv[j], "|") && strcmp(argv[j], ";"))
             j++;
-        if (j<argc && !strcmp(argv[j],"|"))
-                type = 2;
-        if (j<argc && !strcmp(argv[j],";"))
-                type = 1;
+        if (j < argc && !strcmp(argv[j], "|"))
+            type = 2;
+        if (j < argc && !strcmp(argv[j], ";"))
+            type = 1;
         argv[j] = 0;
-        command = &argv[j_prev];      
+        command = &argv[j_prev];
         if (type == 2 && pipe(pipes))
             ft_error();
         pid = fork();
-        if (pid < 0)                                        
+        if (pid < 0)
             ft_error();
         if (pid == 0)
         {
@@ -70,7 +77,7 @@ int main(int argc, char **argv, char **env)
                 ft_cd(command);
             else if (execve(command[0], command, env))
                 ft_msg("error: cannot execute ", command[0]);
-            exit(0);
+            exit (0);
         }
         else
             waitpid(pid, &status, 0);
@@ -78,11 +85,10 @@ int main(int argc, char **argv, char **env)
         {
             dup2(pipes[0], 0);
             close(pipes[1]);
-            close(pipes[0]);
+            close(pipes[0]); 
         }
-        
         if (type == 0)
-            break ;
+            break;
         j++;
     }
     return (0);
